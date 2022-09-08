3632519ab102c1909b6281e4c41b2c6ef38c751f
Tselote Woldemichael <tselotewoldemichael@gmail.com>
2022-09-08 14:55:22 -0400

    rectify

--- components/input.tsx
+++ components/input.tsx
@@ -1,5 +1,5 @@
 export default function Input({ text, func, state }: { text: string, func: any, state: string }) { // TODO type args
   return (
-    <input placeholder={text} onClick={func} value={state} />
+    <input placeholder={text} onChange={func} value={state} />
   );
 }
