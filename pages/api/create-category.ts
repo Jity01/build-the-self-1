36aa410cda959dd0066c1ee9e13a2213a1510214
Tselote Woldemichael <tselotewoldemichael@gmail.com>
2022-09-09 19:33:05 -0400

    add more args

--- pages/api/create-category.ts
+++ pages/api/create-category.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next"
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const category = await createCategory(req.body.title, req.body.shortTitle)
+    const category = await createCategory(req.body.title, req.body.shortTitle, req.body.quote, req.body.sourceOfQuote)
     res.status(200).json(category)
   } catch (e) {
     res.status(500) // TODO make more dynamic
