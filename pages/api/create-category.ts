031e91bddb0e4a7d2785564c358b35e310daa3b9
Tselote Woldemichael <tselotewoldemichael@gmail.com>
2022-09-09 15:50:34 -0400

    make sync

--- pages/api/create-category.ts
+++ pages/api/create-category.ts
@@ -1,9 +1,9 @@
 import { createCategory } from "../../lib/db-script"
 import { NextApiRequest, NextApiResponse } from "next"
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const category = createCategory(req.body.title, req.body.shortTitle)
+    const category = await createCategory(req.body.title, req.body.shortTitle)
     res.status(200).json(category)
   } catch (e) {
     res.status(500) // TODO make more dynamic
