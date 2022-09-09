d39aee991e65f72c3bd83aeed0b0363861395174
Tselote Woldemichael <tselotewoldemichael@gmail.com>
2022-09-09 18:34:47 -0400

    mk functional

--- pages/categories/[id].tsx
+++ pages/categories/[id].tsx
@@ -3,19 +3,21 @@ import { getCategories, getTopicsByCategory, getCategoryTitleById } from '../../
 
 export default function Button({ topics, categoryTitle }) { // TODO type args
   return (
-    <Layout />
+    <Layout>
+      <h2>{categoryTitle}</h2>
+    </Layout> 
   );
 }
 
 
 export async function getStaticPaths() {
   const categories = await getCategories()
-  const paths = categories.map(category => ({ params: { id: category.id }}))
+  const paths = categories.map(category => ({ params: { id: category.id.toString() }}))
   return { paths, fallback: false }
 }
 
 export async function getStaticProps({ params }) {
-  const topics = await getTopicsByCategory(params.id)
-  const categoryTitle = await getCategoryTitleById(params.id)
+  const topics = await getTopicsByCategory(parseInt(params.id))
+  const categoryTitle = await getCategoryTitleById(parseInt(params.id))
   return { props: { topics, categoryTitle } }
 }
