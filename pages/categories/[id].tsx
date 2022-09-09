740e847d58edeaca593b20759470eb220b7dbd70
Tselote Woldemichael <tselotewoldemichael@gmail.com>
2022-09-09 18:19:03 -0400

    init

---
+++ pages/categories/[id].tsx
@@ -0,0 +1,21 @@
+import Layout from '../../components/layout'
+import { getCategories, getTopicsByCategory, getCategoryTitleById } from '../../lib/db-script';
+
+export default function Button({ topics }) { // TODO type args
+  return (
+    <Layout />
+  );
+}
+
+
+export async function getStaticPaths() {
+  const categories = await getCategories()
+  const paths = categories.map(category => ({ params: { id: category.id }}))
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps({ params }) {
+  const topics = await getTopicsByCategory(params.id)
+  const categoryTitle = await getCategoryTitleById(params.id)
+  return { props: { topics, categoryTitle } }
+}
