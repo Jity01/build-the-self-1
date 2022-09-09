cef4119726557c5be288018ef4d0c9b329f8526b
Tselote Woldemichael <tselotewoldemichael@gmail.com>
2022-09-09 19:40:38 -0400

    init

---
+++ pages/api/create-topic.ts
@@ -0,0 +1,12 @@
+import { createTopic } from "../../lib/db-script"
+import { NextApiRequest, NextApiResponse } from "next"
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { title, shortTitle, categoryId } = req.body;
+    const category = await createTopic(title, shortTitle, categoryId);
+    res.status(200).json(category)
+  } catch (e) {
+    res.status(500) // TODO make more dynamic
+  }
+}
