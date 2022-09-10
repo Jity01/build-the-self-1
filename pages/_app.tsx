1d0e5f56726d1f99cc911f3e3cb2303cb7dd31dc
Tselote Woldemichael <tselotewoldemichael@gmail.com>
2022-09-09 20:27:38 -0400

    add global state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,32 @@
 import '../styles/global.css'
+import { useState } from 'react'
 // import { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: any) { // TODO: type props
-  return <Component {...pageProps} />
+  const [openInput, setOpenInput] = useState(false)
+  const [info, setInfo] = useState({
+    title: '', shortTitle: '', quote: '', sourceOfQuote: ''
+  })
+
+  const revealInput = () => setOpenInput(true)
+  const handleChange = (e) => {
+    const { id, value } = e.target
+    setInfo({...info, [id]: value})
+  }
+  const handleReset = () => {
+    setOpenInput(false)
+    setInfo({
+      title: '', shortTitle: '', quote: '', sourceOfQuote: ''
+    })
+  }
+
+  return (
+    <Component
+      {...pageProps}
+      openInput={openInput}
+      info={info}
+      revealInput={revealInput}
+      handleChange={handleChange}
+      handleReset={handleReset}
+   />)
 }
