decdfb343be002a1460109928c3eadca3fe44010
Tselote Woldemichael <tselotewoldemichael@gmail.com>
2022-09-15 12:17:34 -0400

    init

---
+++ components/essays/metadata.tsx
@@ -0,0 +1,10 @@
+export default function Metadata({ source, extraSources, pastEssays, tags }) { // TODO: type props
+  return (
+    <div>
+      <p><em>{source}</em></p>
+      <p><em>{extraSources}</em></p>
+      <p><em>{pastEssays}</em></p>
+      <p><em>{tags}</em></p>
+    </div>
+  )
+}
