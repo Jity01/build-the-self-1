0553eb3478840559cd67635448519ac226052a6a
Tselote Woldemichael <tselotewoldemichael@gmail.com>
2022-09-26 08:47:09 -0400

    type args

--- pages/categories/topics/add-essay.tsx
+++ pages/categories/topics/add-essay.tsx
@@ -2,16 +2,39 @@ import Layout from '../../../components/layout'
 import Button from '../../../components/button'
 import Input from '../../../components/input'
 import Link from 'next/link'
+import {
+  Info,
+  Path,
+  EssayTopic,
+  HandleChange,
+  HandleTextArea,
+  HandleReset,
+  HandleSelect,
+  UpdateEssayTopic
+} from '../../../types/state'
+import { ReactElement } from 'react'
 
+interface Props {
+  path: Path
+  info: Info
+  essayTopic: EssayTopic
+  handleChange: HandleChange
+  handleTextArea: HandleTextArea
+  handleSelect: HandleSelect
+  handleReset: HandleReset
+  updateEssayTopic: UpdateEssayTopic
+};
 export default function EssayForm ({ // TODO turn repo into private - smhhhhhh
   path,
   info,
-  topicIdForAddingAnEssay,
+  essayTopic,
   handleChange,
+  handleTextArea,
+  handleSelect,
   handleReset,
-  updateTopicIdForAddingAnEssay
-}): React.ReactNode { // TODO type args
-  const tagChoices = ['RS', 'RA', 'FU', 'IT', 'EA'] // TODO: abstract it out into a constant folder under utils under user input? easier for typing too..
+  updateEssayTopic
+}: Props): ReactElement { // TODO order args (uniformly)
+  const tagChoices = ['RS-E', 'AN-E', 'INS']
   const stringifyTags = (tags: string[]): string => {
     let value = tags[0]
     for (let i = 1; i < tags.length; i++) {
@@ -26,12 +49,21 @@ export default function EssayForm ({ // TODO turn repo into private - smhhhhhh
     await fetch('/api/create-essay', { // TODO USE QUERY? so that on the url, you are notified of what topic you are on.
       method: 'POST',
       headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
-      body: JSON.stringify({ title, shortTitle, content, source, extraSources, pastEssays, stringifiedTags, topicIdForAddingAnEssay }) // TODO what does this method do anyway?
+      body: JSON.stringify({
+        title,
+        shortTitle,
+        content,
+        source,
+        extraSources,
+        pastEssays,
+        stringifiedTags,
+        essayTopic
+      })
     })
       .then(res => res.status === 200)
       .catch(e => console.log(e))
-    handleReset()
-    updateTopicIdForAddingAnEssay(0)
+    handleReset() // TODO: INCLUDE ESSAY RESETING ?
+    updateEssayTopic(0)
   }
 
   return (
@@ -54,12 +86,12 @@ export default function EssayForm ({ // TODO turn repo into private - smhhhhhh
           <Input id="pastEssays" placeholder="separate (links) by comma" onChange={handleChange} value={info.pastEssays}/>
         </div>
       </div>
-      <div><textarea id="content" placeholder="Input Essay Here" onChange={handleChange} value={info.content} /></div>
-      <div><select id="tags" multiple onChange={handleChange} value={info.tags} size={5}>
+      <div><textarea id="content" placeholder="Input Essay Here" onChange={handleTextArea} value={info.content} /></div>
+      <div><select id="tags" multiple onChange={handleSelect} value={info.tags} size={5}>
         { tagChoices.map((tag, i) => <option key={i}>{tag}</option>)}
       </select></div>
       <Button text="Enter" onClick={handleEnter}/>
-      <div><Link href={'/categories/topics/'.concat(topicIdForAddingAnEssay)}>&larr;Back To Topics</Link></div>
+      <div><Link href={`/categories/topics/${essayTopic}`}>&larr;Back To Topics</Link></div>
       {/* TODO fix link here */}
     </Layout>
   )
