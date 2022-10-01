06449e3ee4429a55f125ba42a42d546dc6771c36
Tselote Woldemichael <tselotewoldemichael@gmail.com>
2022-10-01 14:59:08 -0400

    fix path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,9 @@ import {
   UpdateEssayTopic,
   UpdateMetadataState
 } from '../types/state'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export default function App ({ Component, pageProps }: AppProps): ReactElement { // TODO: maybe..prisma should be at the root
   const [openInput, setOpenInput] = useState(false)
@@ -27,7 +30,7 @@ export default function App ({ Component, pageProps }: AppProps): ReactElement {
     pastEssays: '',
     tags: []
   })
-  const [path, setPath] = useState([['', '']]) // [ [text, url] ]
+  const [path, setPath] = useState<any>([]) // [ [text, url] ] // TODO: look...LISTEN...
   const [essayTopic, setEssayTopic] = useState(0)
   const [metadataState, setMetadataState] = useState(0)
 
@@ -57,13 +60,14 @@ export default function App ({ Component, pageProps }: AppProps): ReactElement {
     })
   }
   const addToPath: AddToPath = (text, url) => setPath([...path, [text, url]])
-  const resetPath: ResetPath = () => setPath([['', '']])
+  const resetPath: ResetPath = () => setPath([])
   const recedePathTo: RecedePathTo = (newPath) => setPath(newPath)
   const updateEssayTopic: UpdateEssayTopic = (topicId) => setEssayTopic(topicId)
   const updateMetadataState: UpdateMetadataState = () => setMetadataState(metadataState + 1)
 
   return (
-    <Component
+    <QueryClientProvider client={queryClient}>
+      <Component
       {...pageProps}
       openInput={openInput}
       metadataState={metadataState}
@@ -80,5 +84,7 @@ export default function App ({ Component, pageProps }: AppProps): ReactElement {
       resetPath={resetPath}
       recedePathTo={recedePathTo}
       updateEssayTopic={updateEssayTopic}
-   />)
+   />
+  </QueryClientProvider>
+  )
 }
